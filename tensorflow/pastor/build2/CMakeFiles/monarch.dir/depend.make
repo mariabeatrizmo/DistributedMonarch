01181968645aa002b44edc8c845f8e1a37a87b3f
mariabeatrizmo <mariabeatrizmo@gmail.com>
2023-05-15 09:07:50 +0100

    commented dht

--- tensorflow/pastor/build2/CMakeFiles/monarch.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for monarch.
-# This may be replaced when dependencies are built.
--- tensorflow/pastor/build2/CMakeFiles/monarch.dir/src/data_plane/logic/remote/remote_stage_builder.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/monarch.dir/src/data_plane/logic/remote/remote_stage_builder.cpp.o: \
- /home/gsd/mbbm/monarch/pastor/src/data_plane/logic/remote/remote_stage_builder.cpp \
- /usr/include/stdc-predef.h
